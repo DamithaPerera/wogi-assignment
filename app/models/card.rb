53c97d33af085aaa81d7868b05b22340f546d828
DamithaPerera <damitha@supplied.eu>
2024-10-02 08:52:50 +0530

    card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ApplicationRecord
-  belongs_to :product
-  belongs_to :client
+    belongs_to :product
+    belongs_to :client
+    def cancel
+        update(cancelled_at: Time.now)
+    end
 end
